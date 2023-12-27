8e53052591d68a8214506f878c9706b639293745
Igor Seliverstov <igrseliwjorstow@bk.ru>
2023-12-27 10:42:55 +0300

    закончил на 31:58: вывел список комнат на клиенте, на сервере добавил фильтрацию roomID, чтобы не возвращались комнаты, которые не создал сокет

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const path = require('path')
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app);
 const io = require('socket.io')(server)
+const {version, validate} = require('uuid')
 
 const ACTIONS = require("./src/socket/actions");
 const PORT = process.env.PORT || 3001;
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 function getClientRooms() {
     const {rooms} = io.sockets.adapter;
 
-    return Array.from(rooms.keys())
+    return Array.from(rooms.keys()).filter(roomID => validate(rooms) && version(roomID) === 4)
 }
 
 function shareRoomsInfo() {
@@ -19,8 +19,60 @@ function shareRoomsInfo() {
     })
 }
 
-io.on('connection', () => {
-    console.log('socket connected')
+io.on('connection', socket => {
+    console.log('Socket connected ', socket.id)
+
+    socket.on(ACTIONS.JOIN, config => {
+        const {room: roomID} = config;
+        const {rooms: joinedRooms} = socket;
+
+        if(Array.from(joinedRooms).includes(roomID)) {
+            return console.warn(`Already joined to ${roomID}`)
+        }
+
+        const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+        clients.forEach(clientID => {
+            io.to(clientID).emit(ACTIONS.ADD_PEER, {
+                peerID: socket.id,
+                createOffer: false
+            })
+
+            socket.emit(ACTIONS.ADD_PEER, {
+                peerID: socket.id,
+                createOffer: true
+            })
+        })
+
+        socket.join(roomID);
+
+        shareRoomsInfo();
+    })
+
+    const leaveRoom = () => {
+        const {rooms} = socket;
+
+        Array.from(rooms).forEach(roomID => {
+            const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+            clients.forEach(clientID => {
+                io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+                    peerID: socket.id,
+                })
+
+                socket.emit(ACTIONS.REMOVE_PEER, {
+                    peerID: clientID,
+                })
+            })
+
+            socket.leave(roomID);
+        })
+
+        shareRoomsInfo();
+    }
+
+    socket.on(ACTIONS.LEAVE, leaveRoom);
+    socket.on('disconnecting', leaveRoom);
 })
 
 server.listen(PORT, () => {
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,37 @@
 import socket from "../../socket";
+import {useEffect, useState} from "react";
+import ACTIONS from "../../socket/actions";
+import {v4} from 'uuid';
+import {useNavigate} from "react-router";
 
 export default function Main() {
+    const navigate = useNavigate();
+    const [rooms, updateRooms] = useState([])
+
+    useEffect(() => {
+        socket.on(ACTIONS.SHARE_ROOMS, ({rooms = []} = {}) => {
+            updateRooms(rooms);
+        })
+    }, [])
+
     return(
-        <div>main</div>
+        <>
+            <h1>Available rooms</h1>
+            <ul>
+                {
+                    rooms.map(roomID => (
+                        <li key={roomID}>
+                            {roomID}
+                            <button onClick={() => {
+                                navigate(`/room/${roomID}`)
+                            }}>Join room</button>
+                        </li>
+                    ))
+                }
+            </ul>
+            <button onClick={() => {
+                navigate(`/room/${v4()}`)
+            }}>Create new room</button>
+        </>
     )
 }
