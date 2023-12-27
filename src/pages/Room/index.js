77a580c8e75a191e6e0ab22e6c90909fb0ec948e
Igor Seliverstov <igrseliwjorstow@bk.ru>
2023-12-27 16:10:01 +0300

    закончил на 44:44: 2 вспомогательных хука для формирования списка пиров и захвата экрана текущего клиента

--- src/pages/Room/index.js
+++ src/pages/Room/index.js
@@ -1,5 +1,27 @@
+import {useParams} from "react-router";
+import useWebRTC, {LOCAL_VIDEO} from "../../hooks/useWebRTC";
+
 export default function Room() {
+    const { id: roomID } = useParams();
+
+    const {clients, provideMediaRef} = useWebRTC(roomID);
+
     return(
-        <div>room</div>
+        <div>
+            {clients.map((clientID) => {
+                return(
+                    <div>
+                        <video
+                            ref={instance => {
+                                provideMediaRef(clientID, instance)
+                            }}
+                            autoPlay
+                            playsInline
+                            muted={clientID === LOCAL_VIDEO}
+                        />
+                    </div>
+                )
+            })}
+        </div>
     )
 }
